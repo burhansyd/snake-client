bb12fc52209ecfa4c67e64ac36e911dfe94c70d4
Burhan Syed <burhan.syed@outlook.com>
2021-07-29 04:48:45 +0000

    implemented full functionality

--- input.js
+++ input.js
@@ -1,14 +1,30 @@
+let connection;
+
+const moves = {
+  w : "Move: up",
+  a : "Move: left",
+  s : "Move: down",
+  d : "Move: right"
+}
+
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
+  for (const button in moves) {
+    if (key === button) {
+      connection.write(moves[button]);
+    }
+  }
 };
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+
   stdin.on("data", handleUserInput);
 
   return stdin;
